dbc0d24c3035a9b4b57f8bebde3901ef1e552192
WahyuDonitya <92677351+WahyuDonitya@users.noreply.github.com>
2023-11-29 18:09:25 +0700

    surat jalan transfer barang done

--- src/layouts/surat-jalan/generate-surat-jalan/index.js
+++ src/layouts/surat-jalan/generate-surat-jalan/index.js
@@ -40,12 +40,18 @@ import dayjs from "dayjs";
 function GenerateSuratJalan() {
   // State
   const [hkeluar, setHkeluar] = useState([]);
+  const [htransfer, setHtransfer] = useState([]);
+  const [selectedHtransfer, setSelectedHtransfer] = useState(null);
+  const [listBarangTransfer, setListBarangTransfer] = useState([]);
+  const [selectedTransferNota, setSelectedTransferNota] = useState(null);
   const [selectedHkeluar, setSelectedHkeluar] = useState(null);
   const [selectedNota, setSelectedNota] = useState(null);
   const [listBarang, setListBarang] = useState([]);
   const [jumlahKirimByItem, setJumlahKirimByItem] = useState({});
+  const [jumlahKirimTransByItem, setJumlahKirimTransByItem] = useState({});
   const [detailSuratJalan, setDetailSuratJalan] = useState([]);
   const [datePicker, setdatePicker] = useState(null);
+  const [datePickerTransfer, setDatePickerTransfer] = useState(null);
 
   // state untuk notification
   const [successSB, setSuccessSB] = useState(false);
@@ -74,32 +80,76 @@ function GenerateSuratJalan() {
         }
       );
       setHkeluar(response.data);
-      console.log("data hkeluar : ", response.data);
+      // console.log("data hkeluar : ", response.data);
+    } catch (error) {
+      console.error("Terjadi kesalahan saat mengambil data HKeluar:", error);
+    }
+  };
+
+  const getHtransfer = async () => {
+    try {
+      const response = await axios.get(
+        "http://127.0.0.1:8000/api/suratjalan/get-htransfer-approved",
+        {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+        }
+      );
+      setHtransfer(response.data);
     } catch (error) {
       console.error("Terjadi kesalahan saat mengambil data HKeluar:", error);
     }
   };
 
   const handleChange = async (newValue) => {
-    setSelectedNota(newValue);
-    setSelectedHkeluar(newValue);
-    try {
-      const response = await axios.get(
-        `http://127.0.0.1:8000/api/transaksi-barang/get-dkeluar/${newValue.hkeluar_id}`,
-        { headers: { Authorization: `Bearer ${accessToken}` } }
-      );
+    if (newValue) {
+      setSelectedNota(newValue);
+      setSelectedHkeluar(newValue);
+      try {
+        const response = await axios.get(
+          `http://127.0.0.1:8000/api/transaksi-barang/get-dkeluar/${newValue.hkeluar_id}`,
+          { headers: { Authorization: `Bearer ${accessToken}` } }
+        );
 
-      console.log("List barang : ", response.data);
-      setListBarang(response.data);
-    } catch (error) {
-      console.error("Terjadi kesalahan saat mengambil data List barang:", error);
+        setListBarang(response.data);
+        console.log("List barang : ", listBarang);
+      } catch (error) {
+        console.error("Terjadi kesalahan saat mengambil data List barang:", error);
+      }
+    } else {
+      setSelectedHkeluar(null);
+      setListBarang([]);
+      setSelectedNota(null);
     }
   };
 
+  const handleChangeHtransfer = async (newValue) => {
+    if (newValue) {
+      setSelectedHtransfer(newValue);
+      setSelectedTransferNota(newValue);
+      console.log(newValue.htransfer_barang_id);
+      try {
+        const response = await axios.get(
+          `http://127.0.0.1:8000/api/suratjalan/get-dtransfer-approved/${newValue.htransfer_barang_id}`,
+          { headers: { Authorization: `Bearer ${accessToken}` } }
+        );
+        setListBarangTransfer(response.data);
+        console.log("List barang transfer ", listBarangTransfer);
+      } catch (error) {
+        console.error("Terjadi kesalahan saat mengambil data List barang:", error);
+      }
+    } else {
+      setSelectedHtransfer(null);
+      setListBarangTransfer([]);
+      setSelectedTransferNota(null);
+    }
+  };
   // End API
 
   useEffect(() => {
     getHkeluar();
+    getHtransfer();
   }, []);
 
   // Function
@@ -142,7 +192,45 @@ function GenerateSuratJalan() {
     }
   };
 
-  // const handleChangeJumlahKirim = (itemId, newValue) => {
+  const addSuratJalantransfer = async () => {
+    try {
+      if (!selectedTransferNota || !datePickerTransfer) {
+        alert("Pastikan nomor nota dan tanggal telah ditentukan");
+        return;
+      }
+
+      const selectedBarang = listBarangTransfer
+        .filter((item) => jumlahKirimTransByItem[item.dtransfer_barang_id] > 0)
+        .map((item) => ({
+          dtransfer_barang_id: item.dtransfer_barang_id,
+          barang_nama: item.barang.barang_nama,
+          detailbarang_batch: item.barang_detail.detailbarang_batch,
+          dtransfer_barang_jumlah: item.dtransfer_barang_jumlah,
+          jumlah_kirim: jumlahKirimTransByItem[item.dtransfer_barang_id],
+        }));
+
+      setDetailSuratJalan(selectedBarang);
+      const dataToSend = {
+        suratjalantransfer_tanggalkirim: datePickerTransfer,
+        htransfer_barang_id: selectedHtransfer.htransfer_barang_id,
+        detail_suratjalantransfer: selectedBarang,
+      };
+
+      console.log(dataToSend);
+
+      const response = await axios.post(
+        "http://127.0.0.1:8000/api/suratjalan/generate-transfer-surat-jalan",
+        dataToSend,
+        { headers: { Authorization: `Bearer ${accessToken}` } }
+      );
+
+      console.log("data to send: ", dataToSend);
+      openSuccessSB();
+    } catch (error) {
+      openErrorSB();
+      console.error("Terjadi kesalahan saat menambahkan data Surat Jalan:", error);
+    }
+  };
   //   // Dapatkan item yang sesuai dengan itemId
   //   const currentItem = listBarang.find((item) => item.dkeluar_id === itemId);
 
@@ -177,6 +265,29 @@ function GenerateSuratJalan() {
 
     if (!updatedIsInputInvalid[itemId]) {
       setJumlahKirimByItem((prev) => ({
+        ...prev,
+        [itemId]: parseInt(newValue),
+      }));
+    }
+  };
+
+  const handleChangeJumlahKirimTransfer = (itemId, newValue) => {
+    const currentItem = listBarangTransfer.find((item) => item.dtransfer_barang_id === itemId);
+
+    // Create a copy of the existing isInputInvalid state
+    const updatedIsInputInvalid = { ...isInputInvalid };
+
+    // Update the validity for the current item
+    if (newValue > currentItem.dtransfer_barang_jumlah_belum_terkirim) {
+      updatedIsInputInvalid[itemId] = true;
+    } else {
+      updatedIsInputInvalid[itemId] = false;
+    }
+
+    setIsInputInvalid(updatedIsInputInvalid);
+
+    if (!updatedIsInputInvalid[itemId]) {
+      setJumlahKirimTransByItem((prev) => ({
         ...prev,
         [itemId]: parseInt(newValue),
       }));
@@ -219,6 +330,58 @@ function GenerateSuratJalan() {
         : item.dkeluar_status === 2
         ? "Belum Terkirim"
         : "Tidak ada harga",
+  }));
+
+  const columnstransfer = [
+    { Header: "Nama Barang", accessor: "barang", width: "10%", align: "left" },
+    { Header: "Batch Barang", accessor: "barang_detail", align: "center" },
+    { Header: "Jumlah Barang", accessor: "dtransfer_barang_jumlah", align: "center" },
+    { Header: "Jumlah Sisa", accessor: "dtransfer_barang_jumlah_belum_terkirim", align: "center" },
+    {
+      Header: "Jumlah Menunggu Approve",
+      accessor: "dtransfer_barang_needapprovekirim",
+      align: "center",
+    },
+    { Header: "Jumlah Terkirim", accessor: "dtransfer_barang_jumlah_terkirim", align: "center" },
+    { Header: "Jumlah Kirim", accessor: "jumlah_kirim", align: "center" },
+    { Header: "Status Barang", accessor: "dtransfer_barang_status", align: "center" },
+  ];
+
+  const rowstransfer = listBarangTransfer.map((item) => ({
+    barang: item.barang.barang_nama,
+    barang_detail: item.barang_detail.detailbarang_batch,
+    dtransfer_barang_jumlah: item.dtransfer_barang_jumlah,
+    dtransfer_barang_jumlah_belum_terkirim: item.dtransfer_barang_jumlah_belum_terkirim,
+    dtransfer_barang_needapprovekirim: item.dtransfer_barang_needapprovekirim,
+    dtransfer_barang_jumlah_terkirim: item.dtransfer_barang_jumlah_terkirim,
+    jumlah_kirim: (
+      <MDInput
+        type="number"
+        value={jumlahKirimTransByItem[item.dtransfer_barang_id] || 0}
+        onChange={(e) => handleChangeJumlahKirimTransfer(item.dtransfer_barang_id, e.target.value)}
+        error={isInputInvalid[item.dtransfer_barang_id]}
+        helperText={
+          isInputInvalid[item.dtransfer_barang_id] ? "Jumlah melebihi stok yang tersedia" : ""
+        }
+        sx={{
+          "& .MuiInput-root": {
+            borderColor: isInputInvalid[item.dtransfer_barang_id] ? "red" : "",
+          },
+        }}
+      />
+    ),
+    dtransfer_barang_status:
+      item.dtransfer_barang_status === 0
+        ? "Sudah Terkirim"
+        : item.dtransfer_barang_status === 1
+        ? "Belum Terkirim"
+        : item.dtransfer_barang_status === 2
+        ? "Terkirim Sebagian"
+        : item.dtransfer_barang_status === 3
+        ? "Ditolak"
+        : item.dtransfer_barang_status === 4
+        ? "Menunggu "
+        : "Tidak ada status",
   }));
 
   // render Notificartion
@@ -345,6 +508,100 @@ function GenerateSuratJalan() {
               </MDBox>
               <Grid item xs={12} px={4} pb={3}>
                 <MDButton variant="gradient" color="success" fullWidth onClick={addSuratJalan}>
+                  Generate Surat Jalan
+                </MDButton>
+              </Grid>
+            </Card>
+          </Grid>
+          <Grid container spacing={6}>
+            {renderSuccessSB}
+            {renderErrorSB}
+          </Grid>
+        </Grid>
+      </MDBox>
+
+      <MDBox pt={6} pb={3}>
+        <Grid container spacing={6}>
+          <Grid item xs={12}>
+            <Card>
+              <MDBox
+                mx={2}
+                mt={-3}
+                py={3}
+                px={2}
+                variant="gradient"
+                bgColor="info"
+                borderRadius="lg"
+                coloredShadow="info"
+              >
+                <MDTypography variant="h6" color="white">
+                  Generate Surat Jalan Transfer Barang
+                </MDTypography>
+              </MDBox>
+              <Grid container>
+                <Grid item xs={6} pt={4} px={2}>
+                  {Array.isArray(htransfer) && htransfer.length > 0 ? (
+                    <Autocomplete
+                      disablePortal
+                      id="combo-box-demo"
+                      options={htransfer}
+                      getOptionLabel={(option) => `${option.htransfer_barang_nota}`}
+                      onChange={(event, newValue) => {
+                        handleChangeHtransfer(newValue);
+                      }}
+                      fullWidth
+                      renderInput={(params) => <TextField {...params} label="Nomor Nota " />}
+                    />
+                  ) : (
+                    <p>Loading customer data...</p>
+                  )}
+                </Grid>
+                <Grid item xs={6} pt={4} px={2}>
+                  <LocalizationProvider dateAdapter={AdapterDayjs}>
+                    <DatePicker
+                      fullWidth
+                      sx={{ width: "100%" }}
+                      value={datePickerTransfer}
+                      onChange={(newValue) => {
+                        // Menggunakan format dayjs untuk mengonversi ke "YYYY-MM-DD"
+                        const formattedDate = newValue ? dayjs(newValue).format("YYYY-MM-DD") : "";
+                        setDatePickerTransfer(formattedDate);
+                      }}
+                    />
+                  </LocalizationProvider>
+                </Grid>
+              </Grid>
+              <br />
+              <Grid container px={3}>
+                <Grid item xs={6}>
+                  <MDTypography variant="h6" fontWeight="medium">
+                    Gudang Asal :{" "}
+                    {selectedHtransfer ? selectedHtransfer.gudang_asal.gudang_nama : "Pilih Nota"}
+                  </MDTypography>
+                </Grid>
+                <Grid item xs={6}>
+                  <MDTypography variant="h6" fontWeight="medium">
+                    Gudang Tujuan :{" "}
+                    {selectedHtransfer ? selectedHtransfer.gudang_tujuan.gudang_nama : "Pilih Nota"}
+                  </MDTypography>
+                </Grid>
+              </Grid>
+              <MDBox pt={3} pb={4}>
+                <DataTable
+                  table={{ columns: columnstransfer, rows: rowstransfer }}
+                  isSorted={false}
+                  entriesPerPage={false}
+                  showTotalEntries={false}
+                  noEndBorder
+                />
+              </MDBox>
+              <Grid item xs={12} px={4} pb={3}>
+                <MDButton
+                  variant="gradient"
+                  color="success"
+                  fullWidth
+                  onClick={addSuratJalantransfer}
+                >
                   Generate Surat Jalan
                 </MDButton>
               </Grid>
